329e540e47b4f6345fdb1d10c63beb29ad1f8d92
Sunday-Okey <sunnybest4500@gmail.com>
2025-04-07 14:07:33 -0400

    added files

--- solution.sql
+++ solution.sql
@@ -30,3 +30,32 @@ SELECT DISTINCT author_id AS id
 FROM Views
 WHERE author_id = viewer_id
 ORDER BY id;
+
+
+-- 4. Invalid Tweets
+
+SELECT tweet_id
+FROM tweets
+WHERE LENGTH(content) > 15;
+
+-- 5. Replace Employee ID With The Unique Identifier
+
+SELECT 
+    eu.unique_id, 
+    e.name 
+FROM 
+    Employees e 
+LEFT JOIN 
+    EmployeeUNI eu ON e.id = eu.id;
+
+
+-- 6. Product Sales Analysis I
+
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM 
+    Sales s
+INNER JOIN 
+    Product p ON s.product_id = p.product_id;
